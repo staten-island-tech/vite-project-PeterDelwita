eac70ace6787ed6034a588206b04360345815f02
PeterDelwita <124073562+PeterDelwita@users.noreply.github.com>
2024-10-29 13:58:51 -0400

    setup

---
+++ app/JS/products.js
@@ -0,0 +1,7 @@
+import "../css/style.css";
+
+// Install Vite, change folder structure
+// Create design system, colors, font, etc.
+// Create array of objects
+// Design a card
+// If node_modules shows up in commits, make another .gitignore file and type node_modules
