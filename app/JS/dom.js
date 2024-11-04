f9f635dd2f0f93a90d5cd9718bb4b42ee0a1cc3c
PeterDelwita <124073562+PeterDelwita@users.noreply.github.com>
2024-11-04 13:57:43 -0500

    sdasdasd

--- app/JS/dom.js
+++ app/JS/dom.js
@@ -1,8 +1,11 @@
 import "../css/style.css";
 import "../css/variables.css";
-import { products } from "./products.js";
 
 const DOMSelectors = {
   items: document.querySelector("item"),
-  buttons: document.querySelector(".button"),
+  buttons: document.querySelector(".btn"),
+  container: document.querySelector(".container"),
+  card: document.querySelector(".card"),
 };
+
+export { DOMSelectors };
--- app/JS/main.js
+++ app/JS/main.js
@@ -1,18 +1,16 @@
 import "../css/style.css";
+import "../css/variables.css";
 import { products } from "./products.js";
 import { DOMSelectors } from "./dom.js";
 
 products.forEach((product) =>
-  insertAdjacentHTML(` 
-  <div>
-    <h1>Wendy's Menu</h1>
-    <div class="container">
-      <div class="card">
-        <h2 class="card-header">${products.name}</h2>
-        <img class="card-image" src="${products.image}" alt="${products.alt}/>
-        <h3 class="card-price">${products.price}</h3>
-      </div>
+  DOMSelectors.container.insertAdjacentHTML(
+    "beforeend",
+    `<div class="card">
+      <h2 class="card-header">${product.name}</h2>
+      <img class="card-image" src="${product.image}" alt="${product.alt}/>
+      <h3 class="card-price">${product.price}</h3>
     </div>
-  </div>
-`)
+`
+  )
 );
