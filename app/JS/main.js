c23990ae63201988a1cffe30c6ae261069c009fe
PeterDelwita <peterd72@nycstudents.net>
2024-11-08 22:15:15 -0500

    css, filters

--- app/JS/main.js
+++ app/JS/main.js
@@ -3,20 +3,19 @@ import "../css/variables.css";
 import { products } from "./products.js";
 import { DOMSelectors } from "./dom.js";
 
-function createProducts() {
-  products.forEach((product) =>
-    DOMSelectors.container.insertAdjacentHTML(
-      // Really should be a variable
-      "beforeend",
-      `<div class="card">
+function createProducts(category) {
+  products
+    .filter((product) => product.category.includes(category))
+    .forEach((product) => {
+      const cardHTML = `<div class="card">
       <h2 class="card-header">${product.name}</h2>
       <img class="card-image" src="${product.image}" alt="${product.alt}"/>
       <h3 class="card-price">Price: ${product.price}</h3>
-    </div>`
-    )
-  );
+    </div>`;
+      DOMSelectors.container.insertAdjacentHTML("beforeend", cardHTML);
+    });
 }
-createProducts();
+createProducts(""); // Will accept anything if the parameter is ""
 
 // Functions for buttons
 function changeTheme() {
@@ -30,17 +29,53 @@ function changeTheme() {
   }
 }
 
-// Use sort, includes, or sum
+// Functions to filter menu items (Use sort, includes, or sum)
 function filterBurgers() {
-  // This function should filter all burgers and make sure they are the only cards on the screen when you click the button
   DOMSelectors.container.innerHTML = "";
-  products.filter((product) => product.category.includes("burger"));
+  createProducts("burger");
 }
 
+function filterSalads() {
+  DOMSelectors.container.innerHTML = "";
+  createProducts("salad");
+}
+
+function filterSides() {
+  DOMSelectors.container.innerHTML = "";
+  createProducts("side");
+}
+
+function filterDrinks() {
+  DOMSelectors.container.innerHTML = "";
+  createProducts("drink");
+}
+
+function resetFilters() {
+  DOMSelectors.container.innerHTML = "";
+  createProducts("");
+}
+
+// Event listeners for functions
 DOMSelectors.themeButton.addEventListener("click", function () {
   changeTheme();
 });
 
 DOMSelectors.burgerButton.addEventListener("click", function () {
   filterBurgers();
+});
+
+DOMSelectors.saladButton.addEventListener("click", function () {
+  filterSalads();
+});
+
+DOMSelectors.sideButton.addEventListener("click", function () {
+  filterSides();
+});
+
+DOMSelectors.drinkButton.addEventListener("click", function () {
+  filterDrinks();
+});
+
+DOMSelectors.filterResetButton.addEventListener("click", function () {
+  resetFilters();
 });
